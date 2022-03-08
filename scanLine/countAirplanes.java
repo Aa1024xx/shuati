2723d6415712cf577bb379ec1c9211145d66e8d6
Aa1024xx <48708642+Aa1024xx@users.noreply.github.com>
2022-03-08 06:54:21 -0800

    Add files via upload

---
+++ scanLine/countAirplanes.java
@@ -0,0 +1,54 @@
+/**
+ * Definition of Interval:
+ * public classs Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+class Point {
+    int time;
+    int flag;
+    Point(int t, int s) {
+        this.time = t;
+        this.flag = s;
+    }
+    public static Comparator<Point> PointComparator = new Comparator<Point>(){
+        public int compare(Point p1, Point p2) {
+            if (p1.time == p2.time) {
+                // > 0 => greater 
+                return p1.flag - p2.flag;
+            }
+            return p1.time - p2.time;
+        }
+    };
+}
+
+public class Solution {
+    /**
+     * @param airplanes: An interval array
+     * @return: Count of airplanes are in the sky.
+     */
+    public int countOfAirplanes(List<Interval> airplanes) {
+        List<Point> list = new ArrayList<>(airplanes.size() * 2);
+        for (Interval i : airplanes) {
+            list.add(new Point(i.start, 1));
+            list.add(new Point(i.end, 0));
+        }
+
+        Collections.sort(list, Point.PointComparator);
+        int count = 0, ans = 0;
+        for (Point p : list) {
+            if (p.flag == 1) {
+                count++;
+            } else {
+                count--;
+            }
+            ans = Math.max(ans, count);
+        }
+
+        return ans;
+    }
+}
